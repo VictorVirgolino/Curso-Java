422e35c7a59463dbe803e3ff212500daad74276b
victorvirgolino <victorvirgolino@gmail.com>
2024-08-08 07:39:34 -0300

    Aula9_07_08_2024 JWT

---
+++ aula9_07_08_2024/jwtme/jwtme/src/main/java/com/jwtme/controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.jwtme.controller;
+
+import com.jwtme.auth.JwtService;
+import com.jwtme.dto.AuthRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @PostMapping("/login")
+    public String login(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        return jwtService.generateToken(userDetails);
+    }
+}
+
+
---
+++ aula9_07_08_2024/jwtme/jwtme/src/test/java/com/jwtme/JwtmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwtme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
