a96e0bf55cda17718a05870f7bfb9e68ea14ad79
victorvirgolino <victorvirgolino@gmail.com>
2024-08-08 07:39:34 -0300

    Aula9_07_08_2024 JWT

---
+++ aula9_07_08_2024/jwtme/jwtme/src/main/java/com/jwtme/auth/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.jwtme.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/home").permitAll() // Permite acesso livre ao /home
+                        .anyRequest().authenticated() // Exige autenticação para qualquer outra rota
+                )
+                .formLogin(); // Usa a página de login padrão do Spring Security
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
